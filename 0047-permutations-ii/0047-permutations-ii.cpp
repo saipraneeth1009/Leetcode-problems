2645f4e9518792e8c5253326ab5b74eb714e3f1c
saipraneeth1009 <168546915+saipraneeth1009@users.noreply.github.com>
2024-06-09 16:23:19 +0530

    Time: 3 ms (88.98%), Space: 10.8 MB (77.57%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-void generatePerm(vector<int> &nums,vector<vector<int>> &ans,vector<int> &check,vector<int> &index){
-    if(check.size()==nums.size()){
-        if(find(ans.begin(),ans.end(),check)==ans.end()) ans.push_back(check);
+void generatePerm(int n,vector<vector<int>> &ans,vector<int> &check,unordered_map<int,int> &count){
+    if(check.size()==n){
+        ans.push_back(check);
         return;
     }
-    for(int i=0;i<nums.size();i++){
-        if(find(index.begin(),index.end(),i)==index.end()){
-            index.push_back(i);
-            check.push_back(nums[i]);
-            generatePerm(nums,ans,check,index);
-            check.pop_back();
-            index.pop_back();
-        }
+    for(auto it:count){
+        int number=it.first;
+        int fre=it.second;
+        if(fre==0) continue;
+        check.push_back(number);
+        count[number]-=1;
+        generatePerm(n,ans,check,count);
+        check.pop_back();
+        count[number]+=1;
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> check,index;
-        generatePerm(nums,ans,check,index);
+        vector<int> check;
+        unordered_map<int,int> count;
+        for(auto it:nums){
+            count[it]+=1;
+        }
+        int n=nums.size();
+        generatePerm(n,ans,check,count);
         return ans;
     }
 };

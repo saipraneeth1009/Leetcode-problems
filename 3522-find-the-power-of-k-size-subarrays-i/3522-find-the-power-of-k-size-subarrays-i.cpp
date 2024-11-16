21cf28ab68e4283aa4fc983f583768f725a30dc4
saipraneeth1009 <168546915+saipraneeth1009@users.noreply.github.com>
2024-11-16 22:53:45 +0530

    Time: 8 ms (17.08%), Space: 34.4 MB (11.17%) - LeetHub

---
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> check;
+        int cur=0;
+        int right=0;
+
+        while(right<nums.size()){
+
+            while(cur<k){
+            
+            if(check.empty()){
+                check.push_back(nums[right]);
+            }
+
+            else if(check.back()+1 == nums[right]){
+                check.push_back(nums[right]);
+            }
+
+            else{
+                check.clear();
+                check.push_back(nums[right]);
+            }
+
+            cur+=1;
+            right+=1;
+            }
+            if(check.size()!=k) ans.push_back(-1);
+            else ans.push_back(check.back());
+
+            if(!check.empty() && check.size()==k) check.pop_front();
+            cur-=1;
+
+        }
+        return ans;
+    }
+};

830485cb86d001586d59194817e6296d5fa16a28
saipraneeth1009 <168546915+saipraneeth1009@users.noreply.github.com>
2024-11-18 19:52:58 +0530

    Time: 76 ms (27.27%), Space: 338.5 MB (20%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int> arr(n+1,0);
 
-        for(int i=0;i<mid;i++){
+        for(int i=0;i<=mid;i++){
             arr[queries[i][0]]+=queries[i][2];
             arr[queries[i][1]+1]-=queries[i][2];
         }
@@ -22,13 +22,13 @@ public:
             if(nums[i]==0) count+=1;
         }
         if(count==nums.size()) return 0;
-        int left=0,right=queries.size();
+        int left=0,right=queries.size()-1;
         int ans=-1;
         while(left<=right){
             int mid = left + (right-left)/2;
 
             if(check(nums,queries,mid)){
-                ans=mid;
+                ans=mid+1;
                 right=mid-1;
             }else{
                 left=mid+1;
